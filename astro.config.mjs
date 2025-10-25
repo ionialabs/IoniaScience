cc2e62cae73787be541f6bd6c56fe7eca46f48e0
ionialabs <admin@ionialabs.com>
2025-10-24 23:21:39 -0500

    fix: set Astro site and Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify'; // or 'static' is fine too
 
 export default defineConfig({
+  site: 'https://science.ionialabs.com',
   output: 'static',        // 'server' or 'static' also fine; hybrid works with functions if needed
   adapter: netlify(),
   integrations: [],
